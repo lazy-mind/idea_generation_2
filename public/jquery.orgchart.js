3d18e989e8ae1abb1ff60eeaf26429d5dfe27332
brucksm <brucksm@stanford.edu>
2021-03-10 17:06:39 -0700

    Update jquery.orgchart.js
    updated "name" field in the UserInfo

--- public/jquery.orgchart.js
+++ public/jquery.orgchart.js
@@ -227,7 +227,7 @@
               return Math.random().toString(36).substr(2, 6);
             };
 
-            const userInfo = { name: "2020-12-01-Full-Launch-MH",
+            const userInfo = { name: "2021-3-10-MB-Creativity_Goal",
                                 success_code: success_code(),
                                 invitation_code: sessionStorage.getItem("invitation_code"),
                                 node_index: node_id_data,
